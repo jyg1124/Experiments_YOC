3b0cd5b1d8d0a84bdea648ff920c1df696179616
jyg1124 <jyg1124@gmail.com>
2017-01-08 19:42:40 +0900

    Create (p449) FSLLN.jl

---
+++ Theories in Analysis of Queues/(p449) FSLLN.jl
@@ -0,0 +1,23 @@
+using Distributions
+using Plots
+pyplot()
+plt = PyPlot
+const TIME = 10.0
+
+function plot_arrival_process(n::Int64)
+  x = [0.0]
+  y = [0.0]
+  while x[length(x)] < TIME
+    push!(x,  x[length(y)] + rand(Pareto(2.095,0.5201))/n  )
+    push!(y, y[length(y)] + 1/n)
+  end
+  plt.xlim(0,10)
+  plt.ylim(0,12)
+  plt.step(x,y)
+end
+
+for i in 1:5
+  plot_arrival_process(100)
+end
+
+show()
