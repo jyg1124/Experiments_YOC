4ac4ab366b61e5253a3c0974aabe4acfca6c5557
Yongkyu Cho <jyg1124@gmail.com>
2017-04-11 15:30:16 +0900

    Update Nonhomogeneous Non-Poisson process.jl

--- Regular queueing simulations/Nonhomogeneous Non-Poisson process.jl
+++ Regular queueing simulations/Nonhomogeneous Non-Poisson process.jl
@@ -1,6 +1,6 @@
 # Written by Yongkyu Cho.
 # Reference: Transforming renewal processes for simulation of nonstationary arrival processes, Gerhardt and Nelson (2009), INFORMS Journal on Computing
-# I use the inversion method.
+# I use the inversion method with Weibull(0.5,0.5) base distribution.
 
 using Distributions, PyPlot, Roots, JuMP, Ipopt
 
